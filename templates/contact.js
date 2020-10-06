9e17e52bea09edfd037a6932d7226dd86bebd5cf
suubh <subhdec99@gmail.com>
2020-10-06 19:43:24 +0530

    Added dark and light mode

--- templates/contact.js
+++ templates/contact.js
@@ -1,3 +1,12 @@
+let checkbox=document.querySelector('input[name=theme]');
+checkbox.addEventListener('change',function(){
+    if(this.checked){
+        document.documentElement.setAttribute('data-theme','dark');
+    }
+    else{
+        document.documentElement.setAttribute('data-theme','light');
+    }
+})
 function sendContact(){
     alert("Thanks for reaching us .");
 }
