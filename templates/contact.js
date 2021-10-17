df2289533738e772de9da7eba5a5b25559f9db1e
musavveer <iammusavveer@gmail.com>
2021-10-17 13:21:43 +0530

    Fixed the bug #42

--- templates/contact.js
+++ templates/contact.js
@@ -6,9 +6,11 @@ let inputMessage = document.querySelector('#input-message');
 checkbox.addEventListener('change',function(){
     if(this.checked){
         document.documentElement.setAttribute('data-theme','dark');
+        document.getElementById('btn-text').innerText = 'Enable Light Mode!';
     }
     else{
         document.documentElement.setAttribute('data-theme','light');
+        document.getElementById('btn-text').innerText = 'Enable Dark Mode!';
     }
 })
 
